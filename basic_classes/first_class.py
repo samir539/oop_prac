4596effa4e6b50a47209e92ccda196851747e1c6
samir <sc9018@ic.ac.uk>
2023-01-26 11:46:03 +0000

    add reset

--- basic_classes/first_class.py
+++ basic_classes/first_class.py
@@ -1,5 +1,9 @@
 class Point():
-    pass
+    def reset(self):
+        self.x = 0
+        self.y = 0
+
+        
 
 p1 = Point()
 p2 = Point()
@@ -9,4 +13,7 @@ p1.y = 2
 p2.x = 3
 p2.y = 4
 
+print(p1.x,p1.y,p2.x,p2.y)
+p1.reset()
+p2.reset()
 print(p1.x,p1.y,p2.x,p2.y)
